f2136df82291b70a7d538ab9316657eeb27c60c0
joel <joel.nipper@charlottenc.gov>
2018-07-06 16:08:26 -0400

    Changing passive buzzer to try to play happy birthday.  Still needs work.

--- 03_passiveBuzzer.py
+++ 03_passiveBuzzer.py
@@ -2,23 +2,31 @@
 import RPi.GPIO as GPIO
 import time
 
-BZRPin = 11
+BZRPin = 12
 
 GPIO.setmode(GPIO.BOARD)       # Numbers pins by physical location
 GPIO.setup(BZRPin, GPIO.OUT)   # Set pin mode as output
 GPIO.output(BZRPin, GPIO.LOW)
 
-p = GPIO.PWM(BZRPin, 50) # init frequency: 50HZ
+p = GPIO.PWM(BZRPin, 50) # init frequency: 50HZ.... speed!
 p.start(50)  # Duty cycle: 50%
 
 try:
 	while True:
-		for f in range(100, 2000, 100):
-			p.ChangeFrequency(f)
-			time.sleep(0.2)
-		for f in range(2000, 100, -100):
-			p.ChangeFrequency(f)
-			time.sleep(0.2)
+                ## Trying to play happy birthday...
+                for f in [400,400,600,400,900,800,1, 400,400,600,400,1100,900,1]:
+                        p.ChangeFrequency(f)
+                        print(f)
+                        time.sleep(0.5)
+                ## Original code        
+# 		for f in range(100, 2000, 100):
+# 			p.ChangeFrequency(f)
+#                         print(f)
+# 			time.sleep(0.5)
+# 		for f in range(2000, 100, -100):
+# 			p.ChangeFrequency(f)
+# 			time.sleep(0.5)
+
 except KeyboardInterrupt:
 	p.stop()
 	GPIO.cleanup()
